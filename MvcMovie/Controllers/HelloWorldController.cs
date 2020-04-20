da51eec8bba99b340e0b59a30f6fadc78f4e5eed
Jaime <j-xile@hotmail..com>
2020-04-19 22:14:03 -0700

    Add a view

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -7,31 +7,45 @@ namespace MvcMovie.Controllers
   {
     // 
     // GET: /HelloWorld/
-
+/*
     public string Index()
     {
       return "This is my default action...";
     }
-/*
-    // 
-    // GET: /HelloWorld/Welcome/ 
-
-    public string Welcome()
-    {
-      return "This is the Welcome action method...";
-    }
 */
+    public IActionResult Index()
+    {
+      return View();
+    }
+    /*
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+          return "This is the Welcome action method...";
+        }
+    */
     // GET: /HelloWorld/Welcome/ 
     // Requires using System.Text.Encodings.Web;
-/*
-    public string Welcome(string name, int numTimes = 1)
+    /*
+        public string Welcome(string name, int numTimes = 1)
+        {
+          return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    */
+    /*
+        public string Welcome(string name, int ID = 1)
+        {
+          return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    */
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
-      return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
-    }
-*/
-    public string Welcome(string name, int ID = 1)
-    {
-      return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+      ViewData["Message"] = "Hello " + name;
+      ViewData["NumTimes"] = numTimes;
+
+      return View();
     }
   }
 }
